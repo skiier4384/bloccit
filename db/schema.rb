bcff7b3cb42124f926c35973878a132c04740483
Xander Miller <xander.miller@gmail.com>
2016-04-27 23:58:43 +0000

    adds options to resolved on questions and refactors specs to use shoulda matchers

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160426154324) do
+ActiveRecord::Schema.define(version: 20160427234649) do
 
   create_table "answers", force: :cascade do |t|
     t.integer  "question_id"
@@ -41,9 +41,9 @@ ActiveRecord::Schema.define(version: 20160426154324) do
   create_table "questions", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
-    t.boolean  "resolved"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.boolean  "resolved",   default: false, null: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
   end
 
 end
