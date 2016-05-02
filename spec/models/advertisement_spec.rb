abb057327dfa5a1a00ad49ab06f6f9fe192b576e
Xander Miller <xander.miller@gmail.com>
2016-05-02 17:33:58 +0000

    advertisement spec uses shoulda matchers

--- spec/models/advertisement_spec.rb
+++ spec/models/advertisement_spec.rb
@@ -1,19 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Advertisement, type: :model do
-    let(:advertisement) {Advertisement.create!}
-    
-    describe "attributes" do
-        it "should respond to title" do
-            expect(advertisement).to respond_to(:title)
-        end
-        
-        it "should respond to copy" do
-            expect(advertisement).to respond_to(:copy)
-        end
-        
-        it "should respond to price" do
-            expect(advertisement).to respond_to(:price)
-        end
-    end
+  let(:advertisement) {Advertisement.create!}
+  
+  describe "attributes" do
+    it {should have_db_column(:title).of_type(:string)}
+    it {should have_db_column(:copy).of_type(:text)}
+    it {should have_db_column(:price).of_type(:integer)}
+  end
 end
