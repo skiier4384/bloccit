e774de10f72b04923b7f2fa849cab4c9e06a4f43
Andrew <rothstein.andrew@gmail.com>
2016-05-13 12:57:19 +0000

    Assignment 42

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -8,6 +8,7 @@ RSpec.describe Comment, type: :model do
  
     it { is_expected.to belong_to(:post) }
     it { is_expected.to belong_to(:user) }
+    it { is_expected.to belong_to(:topic) }
     it { is_expected.to validate_presence_of(:body) }
     it { is_expected.to validate_length_of(:body).is_at_least(5) }
 
