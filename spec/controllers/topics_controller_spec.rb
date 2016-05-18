25b758362576511878fa3437a46f7e15f094a4c6
Andrew <rothstein.andrew@gmail.com>
2016-05-18 19:08:32 +0000

    Checkpoint 46

--- spec/controllers/topics_controller_spec.rb
+++ spec/controllers/topics_controller_spec.rb
@@ -5,6 +5,7 @@ include SessionsHelper
 RSpec.describe TopicsController, type: :controller do
   #let (:my_topic) { Topic.create!(name:  RandomData.random_sentence, description:   RandomData.random_paragraph) }
   let(:my_topic) { create(:topic) }
+  let(:my_private_topic) { create(:topic, public: false) }
 
   context "guest" do
     describe "GET index" do
@@ -15,11 +16,21 @@ RSpec.describe TopicsController, type: :controller do
 
       it "assigns Topic.all to topic" do
         get :index
-        expect(assigns(:topics)).to eq([my_topic])
+        expect(assigns(:topics)).to eq([my_topic, my_private_topic])
+      end
+      
+      it "does not include private topics in @topics" do
+        get :index
+        expect(assigns(:topics)).not_to include(my_private_topic)
       end
     end
 
     describe "GET show" do
+      it "redirects from private topics" do
+         get :show, {id: my_private_topic.id}
+         expect(response).to redirect_to(new_session_path)
+      end
+       
       it "returns http success" do
         get :show, {id: my_topic.id}
         expect(response).to have_http_status(:success)
@@ -72,8 +83,7 @@ RSpec.describe TopicsController, type: :controller do
         delete :destroy, {id: my_topic.id}
         expect(response).to redirect_to(new_session_path)
       end
-    end
-  end
+     end
 
   context "member user" do
     before do
@@ -89,7 +99,7 @@ RSpec.describe TopicsController, type: :controller do
 
       it "assigns Topic.all to topic" do
         get :index
-        expect(assigns(:topics)).to eq([my_topic])
+        expect(assigns(:topics)).to eq([my_topic, my_private_topic])
       end
     end
 
