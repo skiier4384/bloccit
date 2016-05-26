dd98f21aeab1258a9f0902fccfc01b99fc47893c
Xander Miller <xander.miller@gmail.com>
2016-05-26 02:44:58 +0000

    fixes topic scope and spec

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -3,5 +3,5 @@ class Topic < ActiveRecord::Base
   has_many :labelings, as: :labelable
   has_many :labels, through: :labelings
   
-  scope :visible_to, -> (user) { user ? all : where(public: true) }
+  scope :visible_to, -> (user) { user.present? ? all : where(public: true) }
 end
