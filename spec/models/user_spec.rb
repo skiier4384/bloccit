cf275b04b8c90da510998c6aa8f9c0e07ed6b751
Xander Miller <xander.miller@gmail.com>
2016-06-02 19:16:13 +0000

    fixes syntax errors

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,48 +2,27 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   
-    #let(:user) { User.create!(name: "Bloccit User", email: "user@bloccit.com", password: "password") }
-    #let(:user) { create(:user) }
-    #let(:user_with_invalid_name) { build(:user, name: "") }
-    #let(:user_with_invalid_email) { build(:user, email: "") }
+  #let(:user) { User.create!(name: "Bloccit User", email: "user@bloccit.com", password: "password") }
+  #let(:user) { create(:user) }
+  #let(:user_with_invalid_name) { build(:user, name: "") }
+  #let(:user_with_invalid_email) { build(:user, email: "") }
 
   describe "attributes" do #Documentation for shoulda matchers http://matchers.shoulda.io/docs/v3.1.1/
     it {should have_db_column(:email).of_type(:string)}
     it {should have_db_column(:name).of_type(:string)}
     it {should have_db_column(:password_digest).of_type(:string)}
     it {should have_db_column(:role).of_type(:integer).with_options({default: 0, null: false})}
-    it {should define_enum_for(:role).with([:member, :admin])}
+    it {should define_enum_for(:role).with([:member, :moderator, :admin])}
     #expect(user).to have_attributes(name: user.name, email: user.email)
-    
-    it "responds to admin?" do
-       expect(user).to respond_to(:moderator?)
-    end
-    
-    context "moderator user" do
-       before do
-         user.moderator!
-       end
- 
-       it "returns false for #member?" do
-         expect(user.member?).to be_falsey
-       end
-       
-       it "returns true for #moderator?" do
-         expect(user.moderator?).to be_truthy
- 
-       it "returns false for #admin?" do
-         expect(user.admin?).to be_falsey
-       end
-     end
-  end
-  
+  end # describe 'associations' do
+
   describe 'associations' do
     it { should have_many(:posts) }
     it { should have_many(:comments) }
     it { should have_many(:votes) }
     it { should have_many(:favorites) }
-  end
-  
+  end # describe 'associations' do
+
   describe 'validations' do
     # Shoulda tests for name
     it { should validate_presence_of(:name) }
@@ -57,35 +36,35 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of(:password) }
     it { should have_secure_password }
     it { should validate_length_of(:password).is_at_least(6) }
-  end
-  
+  end # describe 'validations' do
+
   describe "#favorite_for(post)" do
-     before do
-       topic = Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph)
-       @post = topic.posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph, user: user)
-     end
- 
-     it "returns `nil` if the user has not favorited the post" do
-       expect(user.favorite_for(@post)).to be_nil
-     end
- 
-     it "returns the appropriate favorite if it exists" do
-       favorite = user.favorites.where(post: @post).create
-       expect(user.favorite_for(@post)).to eq(favorite)
-     end
-   end
-   
-   describe ".avatar_url" do
-     let(:known_user) { create(:user, email: "blochead@bloc.io") }
-     it "returns the proper Gravatar url for a known email entity" do
-       expected_gravatar = "http://gravatar.com/avatar/bb6d1172212c180cfbdb7039129d7b03.png?s=48"
-       expect(known_user.avatar_url(48)).to eq(expected_gravatar)
-     end
-   end
-   
-    describe "#generate_auth_token" do
-     it "creates a token" do
-       expect(user.auth_token).to_not be_nil
-     end
-   end
-end
+    before do
+      topic = Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph)
+      @post = topic.posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph, user: user)
+    end
+  
+    it "returns `nil` if the user has not favorited the post" do
+      expect(user.favorite_for(@post)).to be_nil
+    end
+  
+    it "returns the appropriate favorite if it exists" do
+      favorite = user.favorites.where(post: @post).create
+      expect(user.favorite_for(@post)).to eq(favorite)
+    end
+  end # describe "#favorite_for(post)" do
+
+  describe ".avatar_url" do
+    let(:known_user) { create(:user, email: "blochead@bloc.io") }
+    it "returns the proper Gravatar url for a known email entity" do
+      expected_gravatar = "http://gravatar.com/avatar/bb6d1172212c180cfbdb7039129d7b03.png?s=48"
+      expect(known_user.avatar_url(48)).to eq(expected_gravatar)
+    end
+  end # describe ".avatar_url" do
+
+  describe "#generate_auth_token" do
+    it "creates a token" do
+      expect(user.auth_token).to_not be_nil
+    end
+  end # describe "#generate_auth_token" do
+end # RSpec.describe User, type: :model do
