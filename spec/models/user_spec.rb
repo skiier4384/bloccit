5ab59308ba457243679c490fdd4df9b300a01b20
Andrew <rothstein.andrew@gmail.com>
2016-06-06 19:46:54 +0000

    Fixes The User Spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -37,34 +37,12 @@ RSpec.describe User, type: :model do
     it { should have_secure_password }
     it { should validate_length_of(:password).is_at_least(6) }
   end
-  
-  describe "#favorite_for(post)" do
-     before do
-       topic = Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph)
-       @post = topic.posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph, user: user)
-     end
- 
-     it "returns `nil` if the user has not favorited the post" do
-       expect(user.favorite_for(@post)).to be_nil
-     end
- 
-     it "returns the appropriate favorite if it exists" do
-       favorite = user.favorites.where(post: @post).create
-       expect(user.favorite_for(@post)).to eq(favorite)
-     end
-   end
    
    describe ".avatar_url" do
      let(:known_user) { create(:user, email: "blochead@bloc.io") }
      it "returns the proper Gravatar url for a known email entity" do
        expected_gravatar = "http://gravatar.com/avatar/bb6d1172212c180cfbdb7039129d7b03.png?s=48"
        expect(known_user.avatar_url(48)).to eq(expected_gravatar)
-     end
-   end
-   
-    describe "#generate_auth_token" do
-     it "creates a token" do
-       expect(user.auth_token).to_not be_nil
      end
    end
 end
