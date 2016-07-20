b193be12845d3408fc25a1992ec66233767ac09e
Xander Miller <xander.miller@gmail.com>
2016-07-20 19:08:01 +0000

    fixes syntax error in topic.rb

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -3,8 +3,8 @@ class Topic < ActiveRecord::Base
   has_many :labelings, as: :labelable
   has_many :labels, through: :labelings
   
-  scope: publically_viewable, -> {where(public: true) }
-  scope: privately_viewable, -> {where(public: false) }
+  scope :publically_viewable, -> {where(public: true) }
+  scope :privately_viewable, -> {where(public: false) }
   
   scope :visible_to, -> (user) { user ? all : publically_viewable }
 end
